60710eb80988e4918a66295d2fe41dc5f0b78c9c
tangxiaowei <xiaowei.tang@jimuitech.com>
2021-10-21 18:29:22 +0800

    study

--- public/1/index.js
+++ public/1/index.js
@@ -1,69 +1,15 @@
 window.onload = function () {
     const app = new Vue({
         el: '#app',
-        props: ['ppp'],
         data() {
             return {
                 mmm: 111,
-                aaa: [12, 3, {
-                    bb: 2,
-                }],
             }
         },
-        computed: {
-            ccc() {
-                return this.mmm * 2
-            },
-        },
-        watch: {
-            ccc(newVal, oldVal) {
-                console.log(newVal)
-            },
-        },
-        beforeCreate() {
-            console.log('#app beforeCreate this:', this)
-        },
-        created() {
-            console.log('#app created this:', this)
-        },
         methods: {
-            fff() {
-                console.log('fff')
+            add() {
+                this.mmm += 1
             },
         },
-    })
-}
-
-function testFunction(fn, type) {
-    let obj = {
-        undefined1: undefined,
-        null1: null,
-        string1: 'a',
-        number1: 1,
-        boolean1: true,
-
-        object1: {a: 1, b: 'asd'},
-        array1: [1, 2, 3],
-        function1(aaa) {
-        },
-        reg1: /a/,
-        class1: class bbb {
-        },
-        set1: new Set(),
-        map1: new Map(),
-        objFn: new (function ccc() {
-        })(),
-        objClass: new (class nnn {
-        })(),
-        symbol1: Symbol(12),
-    }
-    Object.keys(obj).forEach(key => {
-        if (type === 1) {
-            console.log(key, fn.call(obj[key]))
-        } else if (type === 2) {
-            console.log(key, typeof obj[key])
-        } else {
-            console.log(key, fn(obj[key]))
-        }
     })
 }
