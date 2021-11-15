1efdaec0dbae73757e7313fc1660027131224cfa
tttxiaowei <tttxiaowei@163.com>
2021-11-15 11:51:16 +0800

    STUDY

--- public/1.js
+++ public/1.js
@@ -1,11 +1,19 @@
-var repeat = function (str, n) {
-      var res = '';
-      while (n) {
-        if (n % 2 === 1) { res += str; }
-        if (n > 1) { str += str; }
-        n >>= 1;
-      }
-      return res
-    };
+// var repeat = function (str, n) {
+//       var res = '';
+//       while (n) {
+//         if (n % 2 === 1) { res += str; }
+//         if (n > 1) { str += str; }
+//         n >>= 1;
+//       }
+//       return res
+//     };
 
-    repeat('a', 5)
+//     repeat('a', 5)
+
+var obj = {}
+Object.defineProperty(obj, 'aaa', {
+    set() {
+        obj.aaa = 12
+    }
+})
+let end
